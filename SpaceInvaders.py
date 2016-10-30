484f158d9149f999fd3ae2cca5b16f4cc47db0c4
Pantelis Dimitroulis <pantelis_pd@outlook.com.gr>
2016-10-30 16:25:43 +0200

    bullet added

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -43,10 +43,6 @@ enemy.setposition(-200, 250)
 
 enemyspeed = 2
 
-#Define bullet state
-
-
-
 #Create the player's bullet
 bullet = turtle.Turtle()
 bullet.color("yellow")
@@ -55,8 +51,15 @@ bullet.penup()
 bullet.speed(0)
 bullet.setheading(90)
 bullet.shapesize(0.5,0.5)
+bullet.hideturtle()
 
-bulletspeed(20)
+bulletspeed = 20
+
+#Define bullet's state
+#ready - ready to fire
+#fire - bullet is firing
+bulletstate = "ready"
+
 
 #Move the player left and right
 def move_left():
@@ -73,11 +76,23 @@ def move_right():
         x = 280
     player.setx(x)
 
+def fire_bullet():
+    #Declare bulletstate as a global if it needs changed
+    global bulletstate
+    if bulletstate == "ready":
+        bulletstate = "fire"
+        #Move the bullet to the just above te player
+        x = player.xcor()
+        y = player.ycor() +10
+        bullet.setposition(x,y)
+        bullet.showturtle()
+
 
 #Create keyboard bindings
 turtle.listen()
 turtle.onkey(move_left, "Left")
 turtle.onkey(move_right , "Right")
+turtle.onkey(fire_bullet,"space")
 
 #Main game loop
 while True:
@@ -100,6 +115,15 @@ while True:
         enemy.sety(y)
         enemyspeed *= -1
 
+    #Move the bullet
+    if bulletstate == "fire":
+        y = bullet.ycor()
+        y += bulletspeed
+        bullet.sety(y)
 
+    #check bullet borders
+    if bullet.ycor() >275:
+        bullet.hideturtle()
+        bulletstate = "ready"
 
 delay = raw_input("Press enter to finish")
